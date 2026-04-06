4f3057c492ae14d142198a38fc97892fd45cfcc7
Raadi12 <124666322+Raadi12@users.noreply.github.com>
2026-04-06 23:35:43 +0700

    editor

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import yt_dlp
+import os
 
 app = FastAPI()
 
-# បើកសិទ្ធិឱ្យ HTML អាចទាញទិន្នន័យបាន (CORS)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,29 +15,25 @@ app.add_middleware(
 @app.get("/download")
 def get_video(url: str):
     try:
-        # បន្ថែមបន្តិចដើម្បីឱ្យទាញយកបានលឿន និងច្បាស់
         ydl_opts = {
             'format': 'best',
             'quiet': True,
             'no_warnings': True,
-            # បន្ថែមជួរខាងក្រោមនេះ
             'nocheckcertificate': True,
-            'ignoreerrors': False,
-            'logtostderr': False,
-            'addheader': [
-                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                'Accept-Language: en-US,en;q=0.9',
-                ],
-            }
+            'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
-            
-            # បងត្រូវកែត្រង់ចំណុច return នេះ
             return {
                 "title": info.get('title', 'Video Download'),
                 "thumbnail": info.get('thumbnail', ''),
                 "download_url": info.get('url')
             }
     except Exception as e:
-        print(f"Error details: {str(e)}") # វានឹងបង្ហាញក្នុង Render Logs
-        return {"error": "Server ជាប់រវល់ ឬ Link មិនត្រឹមត្រូវ។ សូមព្យាយាមម្តងទៀត!"}
+        return {"error": str(e)}
+
+if name == "__main__":
+    import uvicorn
+    # Railway ត្រូវការឱ្យយើងប្រើ Port ដែលវាផ្ដល់ឱ្យតាមរយៈ Environment Variable
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
